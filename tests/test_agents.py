7d21b65b8eefc407c0a93ec4ee1228dd4b368b01
Nathan Woodbury <nathanswoodbury@gmail.com>
2014-11-24 15:05:08 -0700

    Grudge agents implemented and tested.

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -2,7 +2,9 @@ from pyevo.agents import (Agent,
                           AlwaysCooperateAgent,
                           AlwaysDefectAgent,
                           TitForTatAgent,
-                          NotTitForTatAgent)
+                          NotTitForTatAgent,
+                          GrudgeTitForTatAgent,
+                          GrudgeNotTitForTatAgent)
 
 
 class TestAlwaysCooperateAgent(object):
@@ -63,3 +65,105 @@ class TestNotTitForTatAgent(object):
         enemy.last = 'D'
         agent = NotTitForTatAgent('b')
         assert agent.update(enemy) == 'C'
+
+
+class TestGrudgeTitForTatAgent(object):
+
+    def test_first_move_c(self):
+        currenemy = Agent('b')
+        currenemy.last = 'C'
+        agent = GrudgeTitForTatAgent('b')
+        assert agent.update(currenemy) == 'C'
+
+    def test_first_move_d(self):
+        currenemy = Agent('b')
+        currenemy.last = 'D'
+        agent = GrudgeTitForTatAgent('b')
+        assert agent.update(currenemy) == 'C'
+
+    def test_cc(self):
+        lastenemy = Agent('b')
+        lastenemy.last = 'C'
+        currenemy = Agent('b')
+        currenemy.last = 'C'
+        agent = GrudgeTitForTatAgent('b')
+        agent.update(lastenemy)
+        assert agent.update(currenemy) == 'C'
+
+    def test_cd(self):
+        lastenemy = Agent('b')
+        lastenemy.last = 'C'
+        currenemy = Agent('b')
+        currenemy.last = 'D'
+        agent = GrudgeTitForTatAgent('b')
+        agent.update(lastenemy)
+        assert agent.update(currenemy) == 'C'
+
+    def test_dc(self):
+        lastenemy = Agent('b')
+        lastenemy.last = 'D'
+        currenemy = Agent('b')
+        currenemy.last = 'C'
+        agent = GrudgeTitForTatAgent('b')
+        agent.update(lastenemy)
+        assert agent.update(currenemy) == 'D'
+
+    def test_dd(self):
+        lastenemy = Agent('b')
+        lastenemy.last = 'D'
+        currenemy = Agent('b')
+        currenemy.last = 'D'
+        agent = GrudgeTitForTatAgent('b')
+        agent.update(lastenemy)
+        assert agent.update(currenemy) == 'D'
+
+
+class TestGrudgeNotTitForTatAgent(object):
+
+    def test_first_move_c(self):
+        currenemy = Agent('b')
+        currenemy.last = 'C'
+        agent = GrudgeNotTitForTatAgent('b')
+        assert agent.update(currenemy) == 'D'
+
+    def test_first_move_d(self):
+        currenemy = Agent('b')
+        currenemy.last = 'D'
+        agent = GrudgeNotTitForTatAgent('b')
+        assert agent.update(currenemy) == 'D'
+
+    def test_cc(self):
+        lastenemy = Agent('b')
+        lastenemy.last = 'C'
+        currenemy = Agent('b')
+        currenemy.last = 'C'
+        agent = GrudgeNotTitForTatAgent('b')
+        agent.update(lastenemy)
+        assert agent.update(currenemy) == 'D'
+
+    def test_cd(self):
+        lastenemy = Agent('b')
+        lastenemy.last = 'C'
+        currenemy = Agent('b')
+        currenemy.last = 'D'
+        agent = GrudgeNotTitForTatAgent('b')
+        agent.update(lastenemy)
+        assert agent.update(currenemy) == 'D'
+
+    def test_dc(self):
+        lastenemy = Agent('b')
+        lastenemy.last = 'D'
+        currenemy = Agent('b')
+        currenemy.last = 'C'
+        agent = GrudgeNotTitForTatAgent('b')
+        agent.update(lastenemy)
+        assert agent.update(currenemy) == 'C'
+
+    def test_dd(self):
+        lastenemy = Agent('b')
+        lastenemy.last = 'D'
+        currenemy = Agent('b')
+        currenemy.last = 'D'
+        agent = GrudgeNotTitForTatAgent('b')
+        agent.update(lastenemy)
+        assert agent.update(currenemy) == 'C'
